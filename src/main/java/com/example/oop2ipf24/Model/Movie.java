e045d84117b5946c5e6956287821ae0db653b922
Micah <micah.bayley@stu.crc-lennox.qc.ca>
2024-11-25 09:25:48 -0500

    Added code to Movie model.

--- src/main/java/com/example/oop2ipf24/Model/Movie.java
+++ src/main/java/com/example/oop2ipf24/Model/Movie.java
@@ -1,4 +1,15 @@
 package com.example.oop2ipf24.Model;
 
 public class Movie extends Showtime {
+
+    private String aName;
+
+    private String pTime;
+
+    private String pDate;
+
+    public Movie(String pName, String pTime, String pDate) {
+        super(pTime, pDate);
+        this.aName = pName;
+    }
 }
