b63b3774a5897e7d19a7e33737036601ce6a6288
Micah <micah.bayley@stu.crc-lennox.qc.ca>
2024-11-25 19:21:01 -0500

    Added get and set methods to the Showtime model.

--- src/main/java/com/example/oop2ipf24/Model/Showtime.java
+++ src/main/java/com/example/oop2ipf24/Model/Showtime.java
@@ -9,4 +9,20 @@ public class Showtime {
         this.aTime = pTime;
         this.aDate = pDate;
     }
+
+    public String getTime() {
+        return this.aTime;
+    }
+
+    public void setTime(String pTime) {
+        this.aTime = pTime;
+    }
+
+    public String getDate() {
+        return this.aDate;
+    }
+
+    public void setDate(String pDate) {
+        this.aDate = pDate;
+    }
 }
